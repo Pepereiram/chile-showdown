90aa339eb115ef7f2ac8fe0b72ae25da2726ff1b
Cristian Salas <cristian.salas.v@ug.uchile.cl>
2025-09-21 16:04:39 -0300

    Little fix: taba muy gepeteao

--- chilemon-showdown/backend/db.js
+++ chilemon-showdown/backend/db.js
@@ -3,14 +3,8 @@ const path = require("path");
 // Helpers para cargar JSON
 const load = (p) => require(path.join(__dirname, "data", p));
 
-// Puedes mantener por generaciones y luego unir
-const pokemon = [
-  ...load("pokemon/gen1.json"),
-  // ...load("pokemon/gen2.json"),
-  // ...load("pokemon/gen3.json"),
-];
-
 module.exports = () => ({
+  chilemon: load("chilemon.json"),
   users: load("users.json"),
   teams: load("teams.json"),
   teamPokemon: load("teamPokemon.json"), // relación N..M con orden
