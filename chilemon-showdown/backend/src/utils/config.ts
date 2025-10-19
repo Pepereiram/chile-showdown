ca2b40ad920d269545cc707491841ff846b62acf
Pepereiram <jpepereira.m@gmail.com>
2025-10-19 17:30:05 -0300

    ADD: new env and congif

--- chilemon-showdown/backend/src/utils/config.ts
+++ chilemon-showdown/backend/src/utils/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || "localhost";
 
 const MONGODB_URI =
