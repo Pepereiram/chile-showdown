681d2d67713c93f13a2974b21ba57b09c5bd63a8
Pepereiram <jpepereira.m@gmail.com>
2025-10-19 18:50:20 -0300

    FIX: app, long and auth to work with new things

--- chilemon-showdown/backend/src/app.ts
+++ chilemon-showdown/backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { requestLogger, unknownEndpoint, errorHandler } from "./middleware/loggerMiddleware";
 import usersRouter from "./controllers/users";
 import loginRouter from "./controllers/login";
@@ -8,16 +9,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser()); 
 app.use(requestLogger);
+
 // ... routes ...
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.get("/", (_req, res) => {
+  res.type("text/plain").send("Welcome to Chilemon Showdown API!");
+});
 
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 
-app.get("/", (_req, res) => {
-  res.send("Welcome to Chilemon Showdown API!");
-});
+// 404
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 export default app;
